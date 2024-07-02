a24709ede0d880ba8d328a5a5589dead8f7906d0
ericssbn <ericsieben@outlook.com>
2024-07-02 19:08:13 -0300

    OK

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const machineScore = document.querySelector('#machine-score')
 let humanScoreNumber = 0
 let machineScoreNumber = 0
 
+
 const playHuman = (humanChoice) => {
 
     playTheGame(humanChoice, playMachine())
